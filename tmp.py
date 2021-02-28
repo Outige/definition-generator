8b29df8bf5715a04281ea548375bdea2a31484cf
Tieg <tiegosullivanpsnl@gmail.com>
2021-02-28 19:09:30 +0200

    :construction: implimentinf tempplting. Currently in tmp.py

--- tmp.py
+++ tmp.py
@@ -2,6 +2,7 @@ from jinja2 import Template
 import imgkit
 import pandas as pd 
 import math
+import os
 
 def generate_html(html_config):
     with open('static/templates/auto/template0/index.html', 'r') as fp:
@@ -11,20 +12,26 @@ def generate_html(html_config):
         )
         return s
 
-def generate_css():
+def generate_css(css_config):
     with open('static/templates/auto/template0/main.css', 'r') as fp:
-        return fp.read()
+        template_str = Template(fp.read())
+        s = template_str.render(
+            css_config=css_config
+        )
+        return s
 
-def generate_definitions(html_config):
+def generate_definitions(html_config, css_config):
     html = generate_html(html_config)
     with open('static/tmp.html', 'w') as fp:
         fp.write(html)
 
-    css = generate_css()
+    css = generate_css(css_config)
     with open('static/tmp.css', 'w') as fp:
         fp.write(css)
 
     imgkit.from_file('static/tmp.html', 'out%d.jpg'%(0))
+    os.remove("static/tmp.html")
+    os.remove("static/tmp.css")
 
 if __name__ == '__main__':
     html_config = {
@@ -35,10 +42,18 @@ if __name__ == '__main__':
         'image': 'img/3d-modeling.png',
         'title': 'Random Forest',
         'source': 'Source: springboard.com/blog/data-science-terms/whgjgghkkg',
-        'handle': '@conorosullyDS09d890uoh'
+        'handle': '@conorosullyDS09d890uoh',
+        'css': 'tmp.css'
+    }
+    css_config = {
+        'image': {
+            'position': 'right'
+        },
+        'text_size': 35,
+        'title_size': 80,
+        'border_colour': '#0097a7',
+        'title_colour': '#696b6e',
+        'text_colour': '#0097a7'
     }
 
-    generate_definitions(html_config)
-    import os
-    os.remove("static/tmp.html")
-    os.remove("static/tmp.css")
+    generate_definitions(html_config, css_config)
